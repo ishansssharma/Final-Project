c82fce841f89546998cf330e5f1f322fb69d224d
ishansssharma <143998034+ishansssharma@users.noreply.github.com>
2024-05-23 14:53:56 +0200

    create a file for the key

--- src/js/ExchangeRateApi.js
+++ src/js/ExchangeRateApi.js
@@ -7,13 +7,17 @@ const ulList = document.querySelector(".exchangelist")
 //ASYNC FUNCTION FOR API
 const fetchAPI = async () => {
 	try {
-		const response = await fetch("https://v6.exchangerate-api.com/v6/06a031a8a2b698dd2edb02c8/latest/USD");
+		const response = await fetch("https://v6.exchangerate-api.com//latest/USD");
 		const data = await response.json();
 		console.log(data);
 	} catch (error) {
 		console.log("something is wrong"(error));
 	}
 
+
+}
+
+function renderRate = () => {
 
 }
 
---
+++ src/js/apikey.js
@@ -0,0 +1,2 @@
+const API_key = "v6/06a031a8a2b698dd2edb02c8";
+export default API_key
