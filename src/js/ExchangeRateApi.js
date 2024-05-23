6e725e87cf144fdb49ea1ab47b5cb2dbc04a342c
ishansssharma <143998034+ishansssharma@users.noreply.github.com>
2024-05-23 14:24:10 +0200

    fetch variables

---
+++ src/js/ExchangeRateApi.js
@@ -0,0 +1,8 @@
+//VARIABLES FOR THE EXCHANGE RATE SITE
+const nokInput = document.querySelector(".currencyinput");
+const convertButton = document.querySelector(".convertbutton");
+const ulList = document.querySelector(".exchangelist")
+
+
+//ASYNC FUNCTION FOR API
+
