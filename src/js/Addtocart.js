0e75b8f2a7e589403c0f1b049d1ab762886e8f03
ishansssharma <143998034+ishansssharma@users.noreply.github.com>
2024-05-24 20:38:31 +0200

    add features

---
+++ src/js/Addtocart.js
@@ -0,0 +1,4 @@
+//VARIABLES FOR FUNCTION
+const priceClothing = document.querySelectorAll(".price-tag");
+const addToCartButton = document.querySelectorAll(".addtocart-button");
+let totalPrice = 0; 
