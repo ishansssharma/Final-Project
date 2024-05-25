5293f08412a8eb15af84335ef035334e7102af8d
ishansssharma <143998034+ishansssharma@users.noreply.github.com>
2024-05-25 14:32:20 +0200

    checkout¨

---
+++ src/js/firebaseConfig.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyAlhYmK_cDGQh8XA2-N8HJpQB7_OLtiqhQ",
+	authDomain: "final-project-e4942.firebaseapp.com",
+	projectId: "final-project-e4942",
+	storageBucket: "final-project-e4942.appspot.com",
+	messagingSenderId: "555305797200",
+	appId: "1:555305797200:web:e23410195a5590e0bc3355",
+	measurementId: "G-RN8C92FLPW"
+};
+
+export default firebaseConfig
