dcf6abd49fd6304ea442f7d9ef1a241f2879c40a
ishansssharma <143998034+ishansssharma@users.noreply.github.com>
2024-05-25 17:53:40 +0200

    add variables

---
+++ src/js/filterSweater.js
@@ -0,0 +1,10 @@
+//VARIABLES FOR THE FUNCTION
+
+
+
+const filterSweaters = () => {
+	const sustainableButton = document.querySelector(".filter-button");
+	const resetButton = document.querySelector(".reset-button");
+	const sweaters = document.querySelectorAll(".clothing-img")
+
+}
