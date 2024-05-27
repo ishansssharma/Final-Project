35e281d01ca0a639886cd01dbcd2cc5c40213d5f
ishansssharma <143998034+ishansssharma@users.noreply.github.com>
2024-05-28 00:51:44 +0200

    variables for inputs

--- src/js/validateForm.js
+++ src/js/validateForm.js
@@ -1,6 +1,25 @@
+
+//VARIABLES FOR INPUTS 
 const form = document.querySelector(".createaccount-form");
 const fullNameInput = document.querySelector(".fullnameinput");
 const phoneNumberInput = document.querySelector(".phonenumberinput");
 const emailInput = document.querySelector(".emailinput");
 const passwordInput = document.querySelector(".passwordinput");
 
+
+
+
+const errorObject = {
+	fullNameError: "",
+	phoneNumbeError: "",
+	passwordError: "",
+
+
+}
+
+const ErrorsDisplayed = () => {
+
+}
+const formValidation = () => {
+
+}
