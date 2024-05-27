e151948b62c2f10e9dacdc7f886214a60e32f405
ishansssharma <143998034+ishansssharma@users.noreply.github.com>
2024-05-28 01:04:03 +0200

    define text content for every input error

--- src/js/validateForm.js
+++ src/js/validateForm.js
@@ -6,18 +6,30 @@ const phoneNumberInput = document.querySelector(".phonenumberinput");
 const emailInput = document.querySelector(".emailinput");
 const passwordInput = document.querySelector(".passwordinput");
 
+//VARIABLE FOR BUTTON
+const submitButton = document.querySelector(".submitbutton");
 
 
+//VARIABLES FOR ERRORS
+const fullnameErrorSpan = document.querySelector(".fullname-error");
+const phoneNumberErrorSpan = document.querySelector(".phonenumber-error");
+const emailErrorSpan = document.querySelector(".email-error");
+const passwordErrorSpan = document.querySelector(".password-error")
 
 const errorObject = {
 	fullNameError: "",
 	phoneNumbeError: "",
+	emailError: "",
 	passwordError: "",
 
 
 }
 
 const ErrorsDisplayed = () => {
+	fullnameErrorSpan.textContent = errorObject.fullNameError;
+	phoneNumberErrorSpan.textContent = errorObject.phoneNumbeError;
+	emailErrorSpan.textContent = errorObject.emailError;
+	passwordErrorSpan.textContent = errorObject.passwordError;
 
 }
 const formValidation = () => {
