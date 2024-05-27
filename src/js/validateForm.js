01a126e65f08d8acbc720330822d07ba5f6080b7
ishansssharma <143998034+ishansssharma@users.noreply.github.com>
2024-05-28 00:16:21 +0200

    collect variables from html

---
+++ src/js/validateForm.js
@@ -0,0 +1,6 @@
+const form = document.querySelector(".createaccount-form");
+const fullNameInput = document.querySelector(".fullnameinput");
+const phoneNumberInput = document.querySelector(".phonenumberinput");
+const emailInput = document.querySelector(".emailinput");
+const passwordInput = document.querySelector(".passwordinput");
+
